4098774e29af59e1540a44e5b992e9350ca77029
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2025-02-13 13:30:54 +0500

    change

--- components/BorderedButton.tsx
+++ components/BorderedButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BorderedButton = ({ children }: { children: React.ReactNode }) => {
   return (
-    <button className="w-fit border border-black text-[20px] px-[74px] py-[17px]">
+    <button className="m-0 w-fit border border-black px-[74px] py-[17px] text-[20px]">
       {children}
     </button>
   );
